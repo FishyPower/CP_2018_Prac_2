ac494dcbc7a37a06af35f76b4212bc4a60e9c402
FishyPower <35266738+FishyPower@users.noreply.github.com>
2018-02-06 02:57:36 +0800

    Update CP_Prac2_T4.py

--- CP_Prac2_T4.py
+++ CP_Prac2_T4.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[17]:
-
-
 #Computing Practical 2 - Task 4
 
 year = int(input("Input year: "))
@@ -12,4 +6,3 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("Leap")
 else:
     print("Non-Leap")
-
