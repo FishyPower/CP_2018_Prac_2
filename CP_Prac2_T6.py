ee9a2f5c69fbde6afe6e9b2c73c1bd6057b3beba
FishyPower <35266738+FishyPower@users.noreply.github.com>
2018-02-06 02:58:00 +0800

    Update CP_Prac2_T6.py

--- CP_Prac2_T6.py
+++ CP_Prac2_T6.py
@@ -1,13 +1,6 @@
-
-# coding: utf-8
-
-# In[39]:
-
-
-# Computing Practical 2 - Task 6
+#Computing Practical 2 - Task 6
 
 print("Kilograms\tPounds")
 
 for kilograms in range(10):
     print("{0}\t\t{1:.1f}".format(kilograms+1, (kilograms+1) * 2.2))
-
